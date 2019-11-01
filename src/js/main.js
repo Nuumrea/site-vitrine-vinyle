0311e9e2515d0b997b48e4dfa385c2a30f03e94d
Parayre <kevinparayre@mbp-de-parayre.lan>
2019-11-01 16:52:54 +0100

    version final ou pas

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log('it works')
+const hamburger = document.querySelector(".menuAlternatif");
+const menu = document.querySelector("#menuAlternatifOnClick");
+
+hamburger.addEventListener("click", () => {
+  document.body.classList.toggle("open");
+  // alert("Clicked");
+});
